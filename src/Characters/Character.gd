02922f9248b3abdafc0ea174ea1bc857662c3992
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-11-09 21:35:48 +0500

    Created a new resource for stats and motion for Character

--- src/Characters/Character.gd
+++ src/Characters/Character.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 class_name Character
 
+export var res : Resource
+
 func _physics_process(delta):
 	pass
---
+++ src/Resources/character_stats.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+export var speed : float
+export var gravity : float
+export var health : int
+
+export var accelarationRate : float
+export var deccelarationRate : float
+
+var velocity := Vector3.ZERO
+var direction := 0.0
+
