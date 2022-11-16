5600894466ed8eb237caf02cc0c338117935ba1a
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-11-16 14:06:45 +0500

    Deleted setter for direction in stats res

--- src/Resources/character_stats.gd
+++ src/Resources/character_stats.gd
@@ -4,18 +4,9 @@ export var speed : float
 export var gravity : float
 export var health : int
 
-export var accelarationRate : float
-export var deccelarationRate : float
+export var accelerationRate : float
+export var deccelerationRate : float
 
 var velocity := Vector3.ZERO 
-var direction := Vector3.ZERO setget set_direction
+var direction := Vector3.ZERO 
 
-func set_direction(_newDirection):
-	set_velocity_from_direction(_newDirection)
-	return _newDirection
-
-func set_velocity_from_direction(_newDirection : Vector3):
-	
-	velocity.x = _newDirection.x * speed
-	velocity.z = _newDirection.z * speed
-	
